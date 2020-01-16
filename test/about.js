6a9409fb1dc3369dc4b2bd10e85ae368e1c86d2e
Aaron Jauregui <jauregui.aaron@gmail.com>
2020-01-15 23:23:08 -0800

    New icons, new about

--- test/about.js
+++ test/about.js
@@ -1,39 +1,13 @@
 // Used to toggle the menu on smaller screens when clicking on the menu button
 function openNav() {
-	var x = document.getElementById("navSmall");
-	if (x.className.indexOf("w3-show") == -1) {
-		x.className += " w3-show";
-	} else { 
-		x.className = x.className.replace(" w3-show", "");
-	}
+	toggle("navSmall");
 }
 
-//Re-align text on small screen
-$(window).on('resize', function() {
-    if($(window).width() > 600) {
-        $('.aboutTextRight').addClass('w3-margin-left');
-        $('.aboutTextLeft').addClass('w3-margin-right');
-    }else{
-        $('.aboutTextRight').removeClass('w3-margin-left');
-        $('.aboutTextLeft').removeClass('w3-margin-right');
-    }
-})
-$(document).ready(function() {
-    if($(window).width() > 600) {
-        $('.aboutTextRight').addClass('w3-margin-left');
-        $('.aboutTextLeft').addClass('w3-margin-right');
-    }else{
-        $('.aboutTextRight').removeClass('w3-margin-left');
-        $('.aboutTextLeft').removeClass('w3-margin-right');
-    }
-})
-
+//Open accordion panels when aaccordion clicked
 var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
+console.log(acc.length)
+for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    console.log("click")
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -44,6 +18,74 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-$(.accordion).on('click', function() {
 
-    })
+// Toggle an element to show or hide
+function toggle(id) {
+    let element = document.getElementById(id);
+    if (element.className.indexOf("w3-show") == -1) {
+        element.className += " w3-show";
+    } else { 
+        element.className = element.className.replace(" w3-show", "");
+    }
+}
+
+// Hide an element
+function hide(id) {
+    let element = document.getElementById(id);
+    if (element.className.indexOf("w3-show") != -1) {
+        element.className = element.className.replace(" w3-show", "");
+    }
+}
+
+
+// Open the correct story
+function openStory(length) {
+    if(length == "long") {
+        toggle("lifeStory");
+        hide("tldr");
+    }else if(length == "short"){
+        hide("lifeStory");
+        toggle("tldr");
+    }
+}
+
+// Add click event to the story buttons for being active
+var btnContainer = document.getElementById("storyButtons");
+var btns = btnContainer.getElementsByClassName("sb");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    if (this.className.indexOf("sbActive") != -1) {
+        console.log("current button is active")
+        this.className = this.className.replace(" sbActive", "");
+    }else{
+        console.log("current button is not active")
+        var current = document.getElementsByClassName("sbActive");
+        if(current.length > 0) {
+            current[0].className = current[0].className.replace(" sbActive", "");
+        }
+        this.className += " sbActive";
+    }
+  });
+}
+
+//Re-align text when resizing
+$(window).on('resize', function() {
+    if($(window).width() > 600) {
+        $('.aboutTextRight').addClass('w3-margin-left');
+        $('.aboutTextLeft').addClass('w3-margin-right');
+    }else{
+        $('.aboutTextRight').removeClass('w3-margin-left');
+        $('.aboutTextLeft').removeClass('w3-margin-right');
+    }
+})
+//Align text based on screen size at start
+$(document).ready(function() {
+    if($(window).width() > 600) {
+        $('.aboutTextRight').addClass('w3-margin-left');
+        $('.aboutTextLeft').addClass('w3-margin-right');
+    }else{
+        $('.aboutTextRight').removeClass('w3-margin-left');
+        $('.aboutTextLeft').removeClass('w3-margin-right');
+    }
+})
